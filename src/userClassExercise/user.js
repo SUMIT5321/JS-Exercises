c35f29100af9c7d8029591181cbbb07d2c400495
Sumit Raj <sumit.rajrn@gmail.com>
2024-03-05 13:45:24 +0530

    add documentation to User class

--- src/userClassExercise/user.js
+++ src/userClassExercise/user.js
@@ -1,12 +1,21 @@
 import formatText from "../util/stringUtils";
 import strings from "./strings";
 
+/**
+ * User class
+ * @param {String} name - user name
+ * @param {Number} age - user age
+ */
 class User {
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
 
+  /**
+   * @param {User} user - another user to compare this user with
+   * @returns String message
+   */
   compare(user) {
     if (this.age > user.age) return formatText(strings.isOlderText, this.name, user.name);
     if (this.age === user.age) return formatText(strings.sameAgeText, this.name, user.name);
