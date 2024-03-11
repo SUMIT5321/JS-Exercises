72e2eae0d20d423d0924b60883d689ae62a049b0
Sumit Raj <sumit.rajrn@gmail.com>
2024-03-11 11:21:59 +0530

    add oo approach in autoComplete exercise

--- src/JsonAutoComplete/jsonAutoComplete.js
+++ src/JsonAutoComplete/jsonAutoComplete.js
@@ -1,95 +1,106 @@
-const names = [
-  { name: "Luigi Damiano" },
-  { name: "Zenith Coboro" },
-  { name: "Zig Ziglar" },
-  { name: "Steve Costner" },
-  { name: "Bill Grazer" },
-  { name: "Timothy Frazer" },
-  { name: "Boris Becker" },
-  { name: "Glenn Gladwich" },
-  { name: "Jim Jackson" },
-  { name: "Aaron Kabin" },
-  { name: "Roy Goldwin" },
-  { name: "Jason Goldberg" },
-  { name: "Tim Ferris" },
-  { name: "Buck Singham" },
-  { name: "Malcom Gladwell" },
-  { name: "Joy Rabura" },
-  { name: "Vid Luther" },
-  { name: "Tom Glicken" },
-  { name: "Ray Baxter" },
-  { name: "Ari Kama" },
-  { name: "Kenichi Suzuki" },
-  { name: "Rick Olson" },
-];
-const autoResultBox = document.getElementById("autoResult");
-const inputBox = document.getElementById("inputName");
+const autoCompleteSearchHelper = {
+  names: [
+    { name: "Luigi Damiano" },
+    { name: "Zenith Coboro" },
+    { name: "Zig Ziglar" },
+    { name: "Steve Costner" },
+    { name: "Bill Grazer" },
+    { name: "Timothy Frazer" },
+    { name: "Boris Becker" },
+    { name: "Glenn Gladwich" },
+    { name: "Jim Jackson" },
+    { name: "Aaron Kabin" },
+    { name: "Roy Goldwin" },
+    { name: "Jason Goldberg" },
+    { name: "Tim Ferris" },
+    { name: "Buck Singham" },
+    { name: "Malcom Gladwell" },
+    { name: "Joy Rabura" },
+    { name: "Vid Luther" },
+    { name: "Tom Glicken" },
+    { name: "Ray Baxter" },
+    { name: "Ari Kama" },
+    { name: "Kenichi Suzuki" },
+    { name: "Rick Olson" },
+  ],
+  /**
+   * matches input text with name list
+   * @param {String} input
+   * @returns {Array} list of matched names
+   */
+  autocompleteSearch(input) {
+    if (input === "") {
+      return [];
+    }
+    return this.names.filter((term) => term.name.toLowerCase().includes(input.toLowerCase()));
+  },
+};
 
-/**
- * shows option list
- */
-function showOptionList() {
-  if (!autoResultBox.classList.contains("show")) {
-    autoResultBox.classList.add("show");
+class AutoCompleteElement {
+  constructor(inputElement, autoResultElement) {
+    this.inputElement = inputElement;
+    this.autoResultElement = autoResultElement;
+  }
+
+  /**
+   * shows option list
+   */
+  showOptionList() {
+    if (!this.autoResultElement.classList.contains("show")) {
+      this.autoResultElement.classList.add("show");
+    }
+  }
+
+  /**
+   * hides option list
+   */
+  hideOptionList() {
+    if (this.autoResultElement.classList.contains("show")) {
+      this.autoResultElement.classList.remove("show");
+    }
+  }
+
+  /**
+   * Handles option click event
+   * @param {MouseEvent} event
+   */
+  handleOptionClick(event) {
+    const option = event.target;
+    this.inputElement.value = option.innerHTML;
+    this.hideOptionList();
+  }
+
+  /**
+   * Shows the dropdown for given input
+   * @param {MouseEvent} event
+   */
+  showResults(event) {
+    const inputElement = event.target;
+    const input = inputElement.value;
+    const matchedNames = autoCompleteSearchHelper.autocompleteSearch(input);
+
+    const ul = document.createElement("ul");
+    matchedNames.forEach((match) => {
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(match.name));
+      li.addEventListener("click", this.handleOptionClick);
+      ul.appendChild(li);
+    });
+
+    this.autoResultElement.innerHTML = "";
+    this.autoResultElement.appendChild(ul);
+
+    if (matchedNames.length > 0) {
+      this.showOptionList();
+    } else {
+      this.hideOptionList();
+    }
+  }
+
+  registerInputClickListener() {
+    this.inputElement.addEventListener("input", this.showResults.bind(this));
   }
 }
 
-/**
- * hides option list
- */
-function hideOptionList() {
-  if (autoResultBox.classList.contains("show")) {
-    autoResultBox.classList.remove("show");
-  }
-}
-
-/**
- * Handles option click event
- * @param {MouseEvent} event
- */
-function handleOptionClick(event) {
-  const option = event.target;
-  inputBox.value = option.innerHTML;
-  hideOptionList();
-}
-
-/**
- * matches input text with name list
- * @param {String} input
- * @returns {Array} list of matched names
- */
-function autocompleteMatch(input) {
-  if (input === "") {
-    return [];
-  }
-  return names.filter((term) => term.name.toLowerCase().includes(input.toLowerCase()));
-}
-
-/**
- * Shows the dropdown for given input
- * @param {MouseEvent} event
- */
-function showResults(event) {
-  const inputElement = event.target;
-  const input = inputElement.value;
-  const matchedNames = autocompleteMatch(input);
-
-  const ul = document.createElement("ul");
-  matchedNames.forEach((match) => {
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(match.name));
-    li.addEventListener("click", handleOptionClick);
-    ul.appendChild(li);
-  });
-
-  autoResultBox.innerHTML = "";
-  autoResultBox.appendChild(ul);
-
-  if (matchedNames.length > 0) {
-    showOptionList();
-  } else {
-    hideOptionList();
-  }
-}
-
-inputBox.addEventListener("input", showResults);
+const autoCompleteElement = new AutoCompleteElement(document.getElementById("inputName"), document.getElementById("autoResult"));
+autoCompleteElement.registerInputClickListener();
