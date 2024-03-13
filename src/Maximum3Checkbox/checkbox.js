7982f72eb879d3af909741559b85cb56dba81488
Sumit Raj <sumit.rajrn@gmail.com>
2024-03-13 10:10:16 +0530

    use oops for max 3 checkbox exercise

--- src/Maximum3Checkbox/checkbox.js
+++ src/Maximum3Checkbox/checkbox.js
@@ -1,43 +1,57 @@
 /* eslint-disable no-alert */
-const daysCheckboxes = document.getElementsByName("days");
-const noneCheckbox = document.getElementsByName("days-none")[0];
-const selectedDays = [];
-
-/**
- * @param {MouseEvent} event
- */
-function handleFormClick(event) {
-  const view = event.target;
-
-  if (view.type !== "checkbox") {
-    return;
+class FormClickHandler {
+  constructor(daysCheckboxes, noneCheckbox) {
+    this.daysCheckboxes = daysCheckboxes;
+    this.noneCheckbox = noneCheckbox;
+    this.selectedDays = [];
   }
 
-  // if none checked/unchecked
-  if (view.value === "None") {
-    if (view.checked === true) {
-      selectedDays.splice(0, selectedDays.length);
-      daysCheckboxes.forEach((element) => {
-        // eslint-disable-next-line no-param-reassign
+  handleNoneCheckUncheck() {
+    if (this.noneCheckbox.checked === true) {
+      this.selectedDays.splice(0, this.selectedDays.length);
+      this.daysCheckboxes.forEach((element) => {
+      // eslint-disable-next-line no-param-reassign
         element.checked = false;
       });
     }
-    return;
   }
 
-  // days checked / unchecked
-  if (view.checked === true) {
-    if (selectedDays.length === 3) {
-      view.checked = false;
-      alert(`Only 3 days can be selected. You have already selected ${selectedDays[0]}, ${selectedDays[1]} and ${selectedDays[2]}`);
+  /**
+   *
+   * @param {EventTarget} view
+   */
+  handleDaysCheckUncheck(view) {
+    if (view.checked === true) {
+      if (this.selectedDays.length === 3) {
+        view.checked = false;
+        alert(`Only 3 days can be selected. You have already selected ${this.selectedDays[0]}, ${this.selectedDays[1]} and ${this.selectedDays[2]}`);
+      } else {
+        this.noneCheckbox.checked = false;
+        this.selectedDays.push(view.value);
+      }
     } else {
-      noneCheckbox.checked = false;
-      selectedDays.push(view.value);
+      const index = this.selectedDays.indexOf(view.value);
+      this.selectedDays.splice(index, 1);
     }
-  } else {
-    const index = selectedDays.indexOf(view.value);
-    selectedDays.splice(index, 1);
+  }
+
+  /**
+  * @param {MouseEvent} event
+  */
+  handleFormClick(event) {
+    const view = event.target;
+
+    if (view.type !== "checkbox") {
+      return;
+    }
+
+    if (view.value === "None") { // none checked/unchecked
+      this.handleNoneCheckUncheck();
+    } else { // days checked / unchecked
+      this.handleDaysCheckUncheck(view);
+    }
   }
 }
 
-document.forms[0].addEventListener("click", handleFormClick);
+const formClickHandler = new FormClickHandler(document.getElementsByName("days"), document.getElementsByName("days-none")[0]);
+document.forms[0].addEventListener("click", formClickHandler.handleFormClick.bind(formClickHandler));
