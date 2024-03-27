645d9e148515ba60a403bc9620a295d263882485
Sumit Raj <sumit.rajrn@gmail.com>
2024-03-27 12:39:26 +0530

    Update form validation as per feedback

--- src/FormValidationJS/formValidation.js
+++ src/FormValidationJS/formValidation.js
@@ -1,33 +1,69 @@
-const form = document.forms[0];
-
 function showAlert(msg) {
   // eslint-disable-next-line no-alert
   alert(msg);
 }
 
 const validator = {
-  validateString: (text, minLength = 0) => text && text.trim().length > minLength,
-  validateEmail: (email) => {
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailPattern.test(email);
+  emailPattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+  urlPattern: /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/,
+  validateTextLength(...[fieldName, value, minLength]) {
+    return value && value.trim().length >= minLength ? null : `Enter atleast ${minLength} characters in ${fieldName}`;
   },
-  validateUrl: (url) => {
-    const urlPattern = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/;
-    return urlPattern.test(url);
+  validateIsEmpty(...[fieldName, value]) {
+    return (this.validateTextLength(fieldName, value, 1) === null) ? null : `${fieldName} can't be empty.`;
+  },
+  validateEmail(...[, value]) {
+    return this.emailPattern.test(value) ? null : "Enter a valid email";
+  },
+  validateUrl(...[fieldName, value]) {
+    return this.urlPattern.test(value) ? null : `Enter a valid ${fieldName} URL`;
   },
 };
 
-class User {
-  constructor({
-    userId, email, name, timeZone, homePage, aboutMe, receiveCommentsNotification,
-  }) {
-    this.id = userId;
-    this.email = email;
-    this.name = name;
-    this.timeZone = timeZone;
-    this.homePage = homePage;
-    this.aboutMe = aboutMe;
-    this.receiveCommentsNotification = receiveCommentsNotification;
+class UserForm {
+  constructor(form) {
+    this.form = form;
+    this.fieldValues = {};
+  }
+
+  /**
+   * extracts form data and returns an object
+   * @returns {{userId, email, name, timeZone, homePage, aboutMe, receiveCommentsNotification}}
+   */
+  extractFormData() {
+    const formElements = this.form.elements;
+    const formLength = formElements.length;
+    const formData = {};
+
+    for (let i = 0; i < formLength; i += 1) {
+      const element = formElements[i];
+      switch (element.dataset.inputfield) {
+        case "loginId":
+          formData.userId = element.value;
+          break;
+        case "email":
+          formData.email = element.value;
+          break;
+        case "name":
+          formData.name = element.value;
+          break;
+        case "timeZone":
+          formData.timeZone = element.value;
+          break;
+        case "homePage":
+          formData.homePage = element.value;
+          break;
+        case "aboutMe":
+          formData.aboutMe = element.value;
+          break;
+        case "receiveNotification":
+          formData.receiveCommentsNotification = element.checked;
+          break;
+        default:
+          break;
+      }
+    }
+    return formData;
   }
 
   /**
@@ -35,63 +71,61 @@ class User {
    */
   validate() {
     const errorMsgs = [];
-    if (!validator.validateString(this.id)) errorMsgs.push("Login Id can't be empty.");
-    if (!validator.validateEmail(this.email)) errorMsgs.push("Enter a valid email");
-    if (!validator.validateString(this.name)) errorMsgs.push("Name can't be empty.");
-    if (!validator.validateUrl(this.homePage)) errorMsgs.push("Enter a valid Home page URL");
-    if (!validator.validateString(this.aboutMe, 50)) errorMsgs.push("Enter atleast 50 characters about yourself.");
-    if (!this.receiveCommentsNotification) errorMsgs.push("Please check receive notification");
+    function validateField(validatorFunction, ...args) {
+      const errorMessage = validatorFunction.bind(validator)(...args);
+      if (errorMessage != null) errorMsgs.push(errorMessage);
+    }
+
+    const {
+      userId, email, name, timeZone, homePage, aboutMe, receiveCommentsNotification,
+    } = this.fieldValues;
+
+    validateField(validator.validateIsEmpty, "Login Id", userId);
+    validateField(validator.validateIsEmpty, "Email", email);
+    validateField(validator.validateEmail, "Email", email);
+    validateField(validator.validateIsEmpty, "Name", name);
+    validateField(validator.validateIsEmpty, "timeZone", timeZone);
+    validateField(validator.validateIsEmpty, "Home page", homePage);
+    validateField(validator.validateUrl, "Home page", homePage);
+    validateField(validator.validateTextLength, "About me", aboutMe, 50);
+    if (!receiveCommentsNotification) errorMsgs.push("Please check receive comment notifications");
 
     return errorMsgs;
   }
-}
 
-/**
- * handle go button click
- */
-function handleFromSubmit() {
-  const formElements = form.elements;
-  const formLength = formElements.length;
-  const formData = {};
-
-  for (let i = 0; i < formLength; i += 1) {
-    const element = formElements[i];
-    switch (element.id) {
-      case "inputLoginId":
-        formData.userId = element.value;
-        break;
-      case "inputEmail":
-        formData.email = element.value;
-        break;
-      case "inputName":
-        formData.name = element.value;
-        break;
-      case "inputTimeZone":
-        formData.timeZone = element.value;
-        break;
-      case "inputHomePage":
-        formData.homePage = element.value;
-        break;
-      case "inputTextArea":
-        formData.aboutMe = element.value;
-        break;
-      case "cbReceiveNotification":
-        formData.receiveCommentsNotification = element.checked;
-        break;
-      default:
-        break;
+  /**
+   * @param {MouseEvent} event
+   */
+  handleSubmit(event) {
+    this.fieldValues = this.extractFormData();
+    const formErrors = this.validate();
+    if (formErrors.length === 0) {
+      this.form.submit();
+      showAlert("Thanks. Received your details.");
+    } else {
+      event.preventDefault(); // prevent form submission
+      formErrors.forEach((error) => showAlert(error));
     }
+    this.fieldValues = {}; // refresh field values
   }
 
-  const user = new User(formData);
+  /**
+   * method to do any initializations - like add events
+   */
+  init() {
+    this.form.addEventListener("submit", (e) => this.handleSubmit(e));
+  }
 
-  const formErrors = user.validate();
-  if (formErrors.length === 0) {
-    form.submit();
-    showAlert("Thanks. Received your details.");
-  } else {
-    formErrors.forEach((error) => showAlert(error));
+  /**
+   * static method to create {UserForm}
+   * @param {HTMLFormElement} form
+   * @returns {UserForm}
+   */
+  static createUserForm(form) {
+    const userForm = new UserForm(form);
+    userForm.init();
+    return userForm;
   }
 }
 
-document.getElementById("buttonGo").addEventListener("click", handleFromSubmit);
+window.addEventListener("load", () => UserForm.createUserForm(document.forms[0]));
