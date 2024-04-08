3701a8825e1f84ec427ecd0d3cbfd82aa2cdb807
Sumit Raj <sumit.rajrn@gmail.com>
2024-04-08 12:55:32 +0530

    updates for feedback point in calculator

--- src/JSCalculator/calculator.js
+++ src/JSCalculator/calculator.js
@@ -1,40 +1,111 @@
-const display = document.getElementsByName("display")[0];
+// regex
+const REGEX_VALUE_AND_OPERATORS = /[\d.*+\-/]/;
 
-/**
- * Handles click on form element
- * @param {MouseEvent} event
- * @returns nothing
- */
-function handleClick(event) {
-  const view = event.target;
+// calculator constants
+const CAL_AC = "AC";
+const CAL_DE = "DE";
+const CAL_DOT = ".";
+const CAL_DIVIDE = "/";
+const CAL_MUL = "*";
+const CAL_MINUS = "-";
+const CAL_PLUS = "+";
+const CAL_EQUAL = "=";
 
-  if (view.type !== "button") return;
-  const { value } = view;
-
-  if (value === "AC") {
-    display.value = "";
-    return;
+// Calculator class
+class Calculator {
+  constructor({ containerId }) {
+    this.container = document.querySelector(containerId);
+    this.#init();
   }
 
-  if (value === "DE") {
-    display.value = display.value.toString().slice(0, -1);
-    return;
+  #init() {
+    const createFirstRow = () => {
+      const displayDiv = document.createElement("div");
+      displayDiv.classList.add("display");
+      const displayInput = document.createElement("input");
+      displayDiv.appendChild(displayInput);
+      return { displayDiv, displayInput };
+    };
+
+    const createRow = (...buttons) => {
+      const rowDiv = document.createElement("div");
+      buttons.forEach((element) => {
+        const button = document.createElement("input");
+        button.type = "button";
+        button.value = element;
+        button.onclick = this.handleClick.bind(this);
+        rowDiv.appendChild(button);
+      });
+      return rowDiv;
+    };
+
+    const createLastRow = () => {
+      const lastRow = createRow("00", 0, CAL_EQUAL);
+      lastRow.lastElementChild.classList.add("equal");
+      return lastRow;
+    };
+
+    // create innerContainer
+    const innerContainer = document.createElement("div");
+    innerContainer.classList.add("calculator");
+
+    // create all rows in the calculator
+    const { displayDiv, displayInput } = createFirstRow();
+    this.display = displayInput;
+    innerContainer.appendChild(displayDiv);
+    innerContainer.appendChild(createRow(CAL_AC, CAL_DE, CAL_DOT, CAL_DIVIDE));
+    innerContainer.appendChild(createRow(7, 8, 9, CAL_MUL));
+    innerContainer.appendChild(createRow(4, 5, 6, CAL_MINUS));
+    innerContainer.appendChild(createRow(1, 2, 3, CAL_PLUS));
+    innerContainer.appendChild(createLastRow());
+
+    // add inner container to the calculator container
+    this.container.appendChild(innerContainer);
   }
 
-  if (value === "=") {
-    try {
-      // eslint-disable-next-line no-eval
-      display.value = eval(display.value);
-    } catch (error) {
-      display.value = "error";
+  /**
+   * Handles click on form element
+   * @param {MouseEvent} event
+   * @returns nothing
+   */
+  handleClick(event) {
+    const view = event.target;
+
+    if (view.type !== "button") return;
+    const { value } = view;
+
+    if (value === CAL_AC) {
+      this.display.value = "";
+      return;
     }
-    return;
+
+    if (value === CAL_DE) {
+      this.display.value = this.display.value.toString().slice(0, -1);
+      return;
+    }
+
+    if (value === CAL_EQUAL) {
+      try {
+        // eslint-disable-next-line no-eval
+        this.display.value = eval(this.display.value);
+      } catch (error) {
+        this.display.value = "error";
+      }
+      return;
+    }
+
+    if (REGEX_VALUE_AND_OPERATORS.test(value)) {
+      this.display.value += value;
+    }
   }
 
-  const validValuePattern = /[\d.*+\-/]/g;
-  if (validValuePattern.test(value)) {
-    display.value += value;
+  // static helper method to create calculator
+  static create({ containerId }) {
+    const calculator = new Calculator({ containerId });
+    return calculator;
   }
 }
 
-document.forms[0].addEventListener("click", handleClick);
+window.addEventListener("load", () => {
+  Calculator.create({ containerId: "[data-container='calculator']" });
+});
